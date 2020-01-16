b7257844fc91bdc7cdf78d3583c623004ed1589a
Enayatullah Naseri <enayatullahna@gmail.com>
2020-01-15 18:35:51 -0800

    Created albums project

---
+++ Albums/Albums/Resources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Albums
+//
+//  Created by Enayatullah Naseri on 1/15/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
